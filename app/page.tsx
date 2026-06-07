dca5ffb7896859a426cf86a3b5439df7c9404bec
Megha <meghabhattacharya28@gmail.com>
2026-06-07 14:59:26 +0530

    phonenumber change

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function HomePage() {
       <Navbar />
       <a
         className="fixed bottom-10 right-10 z-50 cursor-pointer  rounded-full p-2   hover:shadow-[0_12px_40px_rgb(0,0,0,0.25)]   hover:-translate-y-1 transition-all duration-300 ease-out  text-red-800"
-        href="https://wa.me/919871600780?text=Hello%2C%20I%27d%20like%20to%20know%20more!"
+        href="https://wa.me/919871900780?text=Hello%2C%20I%27d%20like%20to%20know%20more!"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Chat on WhatsApp"
